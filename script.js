65b793aa43dc82e949208877fefe66220518cf36
Yaroslav <yaroslav.kramarchuk@gmail.com>
2023-08-08 16:14:02 +0200

    mobile version added

--- script.js
+++ script.js
@@ -25,3 +25,51 @@ function handleScroll() {
 
 // Attach scroll event listener
 window.addEventListener("scroll", handleScroll);
+
+// Get the button:
+const mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.addEventListener("scroll", () => {
+	if (
+		document.body.scrollTop > 500 ||
+		document.documentElement.scrollTop > 500
+	) {
+		mybutton.classList.add("visible");
+	} else {
+		mybutton.classList.remove("visible");
+	}
+});
+
+// Smooth scroll to the top of the document
+mybutton.addEventListener("click", () => {
+	window.scrollTo({
+		top: 0,
+		behavior: "smooth",
+	});
+});
+
+// ul columns
+
+function updateColumns() {
+	column = 0;
+	columnItems.each(function (idx, el) {
+		if (column > columns.length) {
+			column = 0;
+		}
+		$(columns.get(column)).append(el);
+		column += 1;
+	});
+}
+
+// burger menu
+
+const burger = document.querySelector(".burger");
+const navList = document.querySelector(".nav-list");
+const container = document.querySelector(".container");
+
+burger.addEventListener("click", () => {
+	burger.classList.toggle("active");
+	navList.classList.toggle("active");
+	// container.classList.toggle("expanded");
+});
